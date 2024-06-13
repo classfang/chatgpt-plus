d2d48de008af21361db2d18404d2dc77c5cf1dbe
Junki <fangjunjievip@hotmail.com>
2024-06-13 16:56:06 +0800

    update

--- src/renderer/src/components/ChatGPTBodyEmpty.vue
+++ src/renderer/src/components/ChatGPTBodyEmpty.vue
@@ -1,10 +1,23 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import chatgptAvatar from '@renderer/assets/image/chatgpt-avatar.png'
+</script>
 
 <template>
-  <div class="chatgpt-body-empty"></div>
+  <div class="chatgpt-body-empty">
+    <el-avatar :size="50" :src="chatgptAvatar" />
+    <div>{{ $t('app.chatgpt.body.empty.content') }}</div>
+  </div>
 </template>
 
 <style lang="scss" scoped>
 .chatgpt-body-empty {
+  height: 100%;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: $app-padding-large;
+  font-size: var(--el-font-size-large);
 }
 </style>
