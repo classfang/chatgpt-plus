88f678b45d2a1d256aeb8bf3aff791714f79c227
Junki <fangjunjievip@hotmail.com>
2024-06-15 11:47:03 +0800

    update

--- src/renderer/src/utils/markdown-util.ts
+++ src/renderer/src/utils/markdown-util.ts
@@ -11,6 +11,9 @@ import markdownItMathjax3 from 'markdown-it-mathjax3'
 // 多语言
 const { t } = i18n.global
 
+// 结尾标识
+const endFlag = '【end】'
+
 // 代码复制
 const clipboard = new ClipboardJS('.code-header-copy', {
   text: function (trigger) {
@@ -25,21 +28,27 @@ clipboard.on('success', () => {
 // MarkdownIt
 const markdown = new MarkdownIt({
   highlight: (str: string, lang: string) => {
-    if (!lang) {
+    // 可能带有 endFlag
+    let insertEndFlag = false
+    if (lang && lang.endsWith(endFlag)) {
+      lang = lang.replace(endFlag, '')
+      insertEndFlag = true
+    }
+
+    // 不支持的 lang 统一使用 text
+    if (!hljs.getLanguage(lang)) {
       lang = 'text'
     }
-    let codeHtml = `<code class="hljs language-${lang}">${
-      hljs.highlight(str, { language: lang }).value
-    }</code>`
-    codeHtml =
-      `<div class="code-header">
+
+    return `<pre><div class="code-header">
         <div>${lang}</div>
         <div class="code-header-copy" data-clipboard-text-base64='${textToBase64(str)}'>
           <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="icon-sm"><path fill-rule="evenodd" clip-rule="evenodd" d="M12 4C10.8954 4 10 4.89543 10 6H14C14 4.89543 13.1046 4 12 4ZM8.53513 4C9.22675 2.8044 10.5194 2 12 2C13.4806 2 14.7733 2.8044 15.4649 4H17C18.6569 4 20 5.34315 20 7V19C20 20.6569 18.6569 22 17 22H7C5.34315 22 4 20.6569 4 19V7C4 5.34315 5.34315 4 7 4H8.53513ZM8 6H7C6.44772 6 6 6.44772 6 7V19C6 19.5523 6.44772 20 7 20H17C17.5523 20 18 19.5523 18 19V7C18 6.44772 17.5523 6 17 6H16C16 7.10457 15.1046 8 14 8H10C8.89543 8 8 7.10457 8 6Z" fill="currentColor"></path></svg>
           <span>Copy</span>
         </div>
-      </div>` + codeHtml
-    return `<pre>${codeHtml}</pre>`
+      </div><code class="hljs language-${lang}">${
+        hljs.highlight(str, { language: lang }).value
+      }${insertEndFlag ? endFlag : ''}</code></pre>`
   }
 })
 
@@ -53,7 +62,6 @@ export const renderMarkdown = (content: string, isLoading: boolean) => {
   }
 
   // 加载中，显示闪烁光标
-  const endFlag = '【end】'
   let htmlCode = markdown.render(content + endFlag)
   // 找到结束标识
   const endFlagIndex = htmlCode.lastIndexOf(endFlag)
