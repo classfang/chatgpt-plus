30292701eaa4478919d5b041a07977f9ff2bfa75
Junki <fangjunjievip@hotmail.com>
2024-06-19 16:21:02 +0800

    feat: Image input

--- src/renderer/src/components/ChatGPTMessageUser.vue
+++ src/renderer/src/components/ChatGPTMessageUser.vue
@@ -9,7 +9,21 @@ const message = defineModel<ChatMessage>('message', {
 
 <template>
   <div class="chatgpt-message-user">
-    <div class="message-content select-text">{{ message.content }}</div>
+    <div class="message-content select-text">
+      <span>{{ message.content }}</span>
+      <div v-if="message.images && message.images.length > 0" class="file-list">
+        <div v-for="(att, index) in message.images" :key="att.path" class="file-item">
+          <el-image
+            class="item-image"
+            :src="`file://${att.path}`"
+            :preview-src-list="message.images.map((a) => `file://${a.path}`)"
+            :initial-index="index"
+            fit="cover"
+          />
+        </div>
+      </div>
+    </div>
+
     <ChatGPTMessageConsole v-model:message="message" class="message-console" />
   </div>
 </template>
@@ -35,7 +49,34 @@ const message = defineModel<ChatMessage>('message', {
     border-radius: calc(1.5rem / 2 + $app-padding-small);
     line-height: 1.3rem;
     display: flex;
-    justify-content: center;
+    flex-direction: column;
+    gap: $app-padding-small;
+
+    .file-list {
+      display: flex;
+      gap: $app-padding-small;
+      flex-wrap: wrap;
+
+      .file-item {
+        height: 60px;
+        width: 60px;
+        position: relative;
+
+        .item-image {
+          height: 100%;
+          width: 100%;
+          border-radius: $app-border-radius-base;
+        }
+
+        .item-close-btn {
+          height: $app-icon-size-small;
+          width: $app-icon-size-small;
+          position: absolute;
+          top: calc($app-icon-size-small / -2);
+          right: calc($app-icon-size-small / -2);
+        }
+      }
+    }
   }
 
   .message-console {
