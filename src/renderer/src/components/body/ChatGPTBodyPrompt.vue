dd5eb9f3fb54d6787b522610f58e8ff0675442a0
Junki <fangjunjievip@hotmail.com>
2024-06-21 11:12:10 +0800

    feat: Prompt Library

--- src/renderer/src/components/body/ChatGPTBodyPrompt.vue
+++ src/renderer/src/components/body/ChatGPTBodyPrompt.vue
@@ -3,29 +3,41 @@ import { MoreFilled } from '@element-plus/icons-vue'
 import prompts from '@renderer/assets/json/prompts.json'
 import { useAppSettingStore } from '@renderer/store/app-setting'
 import { getRandomElements } from '@renderer/utils/array-util'
+import { reactive, toRefs } from 'vue'
 
 // 仓库
 const appSettingStore = useAppSettingStore()
 
 // 事件
 const emits = defineEmits(['use-prompt'])
+
+// 数据绑定
+const data = reactive({
+  randomPrompts: getRandomElements(prompts[appSettingStore.app.locale], 4),
+  dialogVisible: false
+})
+const { randomPrompts, dialogVisible } = toRefs(data)
 </script>
 
 <template>
   <div class="chatgpt-body-prompt">
     <div
-      v-for="p in getRandomElements(prompts[appSettingStore.app.locale], 4)"
+      v-for="p in randomPrompts"
       :key="p[0]"
       class="prompt-item"
       @click="emits('use-prompt', p[1])"
     >
       {{ p[0] }}
     </div>
-    <div class="prompt-item">
+    <div class="prompt-item" @click="dialogVisible = true">
       <el-icon>
         <MoreFilled />
       </el-icon>
     </div>
+
+    <el-dialog v-model="dialogVisible" :title="$t('app.chatgpt.body.prompt.title')" width="700">
+      <div class="dialog-body"></div>
+    </el-dialog>
   </div>
 </template>
 
@@ -58,6 +70,10 @@ const emits = defineEmits(['use-prompt'])
       background-color: var(--el-fill-color-dark);
       color: var(--el-text-color);
     }
+  }
+
+  .dialog-body {
+    height: $app-dialog-height;
   }
 }
 </style>
